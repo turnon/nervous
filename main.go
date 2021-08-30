11f3205f3e6fc2e996a7247ea116aeda5b9dd5a5
ken <block24block@gmail.com>
2021-08-30 15:42:52 +0000

    fill events into template

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/turnon/nervous/views"
 )
 
+type jsonObject struct {
+	obj interface{}
+}
+
+func (j *jsonObject) EvalObject() string {
+	bytes, _ := json.Marshal(j.obj)
+	return "<pre style='display:none'>" +
+		string(bytes) +
+		"</pre><script>var obj = JSON.parse(document.querySelector('pre').innerText)</script>"
+}
+
+type calendarPage struct {
+	Events []event `json:"events"`
+}
+
+type event struct {
+	Title string `json:"title"`
+	Start string `json:"start"`
+	End   string `json:"end"`
+}
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		bytes, _ := views.Render("cal.html", nil)
+		calPage := calendarPage{Events: []event{
+			{Title: "aaa", Start: "2021-08-21"},
+			{Title: "aaa", Start: "2021-08-25", End: "2021-08-27"},
+		}}
+
+		bytes, _ := views.Render("cal.html", &jsonObject{calPage})
 		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
 		return c.Send(bytes)
 	})
